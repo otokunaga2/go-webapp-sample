9a346889d60214921aad71bd5d89e9d2d3a218e6
Seiki Tokunaga <otokuna46@gmail.com>
2022-08-20 18:19:01 +0900

    Add spa template file

--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module otokunaga2/m
 go 1.13
 
 require (
-	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/lib/pq v1.10.6 // indirect
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible
+	github.com/ziutek/mymysql v1.5.4 // indirect
 	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8 // indirect
+	gopkg.in/gorp.v1 v1.7.2
 )
--- main.go
+++ main.go
@@ -1,17 +1,69 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
+	_ "github.com/mattn/go-sqlite3"
+	"gopkg.in/gorp.v1"
 )
+
+type Comment struct {
+	Id      int64     `json:"id" db:"id,primarykey,autoincrement"`
+	Name    string    `json:"nane" db:"name,notnull,default:'noname',size:200"`
+	Text    string    `json:"text" db:"text,notnull,size:399"`
+	Created time.Time `json:"created" db:"created,notnull"`
+	Updated time.Time `json:"updated" db:"updated,notnull"`
+}
 
 func main() {
 	fmt.Println("vim-go")
 	e := echo.New()
+	dbmap := initDb()
+	defer dbmap.Db.Close()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "hello world")
 	})
+	e.GET("/api/comments", func(c echo.Context) error {
+		var comments []Comment
+		_, err := dbmap.Select(&comments, "SELECT * FROM comments ORDER by created desc LIMIT 10")
+		if err != nil {
+			c.Logger().Error("Select: ", err)
+			return c.String(http.StatusBadRequest, "Select: "+err.Error())
+		}
+		return c.JSON(http.StatusOK, comments)
+	})
+	e.Static("/", "static/")
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func initDb() *gorp.DbMap {
+	// connect to db using standard Go database/sql API
+	// use whatever database/sql driver you wish
+	db, err := sql.Open("sqlite3", "/tmp/post_db.bin")
+	checkErr(err, "sql.Open failed")
+
+	// construct a gorp DbMap
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.SqliteDialect{}}
+
+	// add a table, setting the table name to 'posts' and
+	// specifying that the Id property is an auto incrementing PK
+	dbmap.AddTableWithName(Comment{}, "comments").SetKeys(true, "Id")
+
+	// create the table. in a production system you'd generally
+	// use a migration tool, or create the tables via scripts
+	err = dbmap.CreateTablesIfNotExists()
+	checkErr(err, "Create tables failed")
+
+	return dbmap
+}
+
+func checkErr(err error, msg string) {
+	if err != nil {
+		log.Fatalln(msg, err)
+	}
 }
